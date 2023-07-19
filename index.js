12f9838fe9c722fc00a83946306bf3336f1b26fa
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-20 00:40:31 +0300

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const divide = (number1, number2) => number1 / number2;
+
+
+const square = (number) => number * number;
+
+
+const add = (number1, number2) => number1 + number2;
+
+
+module.exports = {
+  divide,
+  square,
+  add,
+};
